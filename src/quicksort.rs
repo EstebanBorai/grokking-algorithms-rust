58419c63ef7d885615dbfdb45760a32ba8f8d76f
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-12 18:24:03 -0400

    refactor: Refactor quicksort implementation (#2)
    - [x] Allow repeat items

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -23,29 +23,22 @@ where
     // This is the recursive case where we plan
     // to minimize the size of the problem thus
     // applying a D&C mindset
-    let pivot = coll_clone.get(0).unwrap();
-    let mut less_than_pivot: Vec<T> = quicksort(
-        &coll_clone
-            .clone()
-            .into_iter()
-            .filter(|i| *i < *pivot)
-            .collect::<Vec<T>>(),
-    );
-    let greather_than_pivot: Vec<T> = quicksort(
-        &coll_clone
-            .clone()
-            .into_iter()
-            .filter(|i| *i > *pivot)
-            .collect::<Vec<T>>(),
-    );
+    let pivot = coll_clone.first().unwrap();
 
-    less_than_pivot.push(*pivot);
+    let less_than_pivot: Vec<T> = quicksort(&coll.iter().filter(|i| *i < pivot).cloned().collect());
+    let equal_to_pivot: Vec<T> = coll.iter().filter(|i| *i == pivot).cloned().collect();
+    let mut greater_than_pivot: Vec<T> =
+        quicksort(&coll.iter().filter(|i| *i > pivot).cloned().collect());
 
-    less_than_pivot
-        .iter()
-        .chain(greather_than_pivot.iter())
-        .copied()
-        .collect()
+    // Start building the result with the elements less than the pivot
+    let mut result = less_than_pivot;
+
+    // Add the elements equal to the pivot to the result
+    result.extend(equal_to_pivot);
+    // Add the elements greater than the pivot to the result
+    result.append(&mut greater_than_pivot);
+
+    result
 }
 
 #[cfg(test)]
@@ -58,5 +51,13 @@ mod tests {
         let result = quicksort(&items);
 
         assert_eq!(result, vec![2, 4, 5, 6, 7, 8, 10]);
+    }
+
+    #[test]
+    fn sorts_a_collection_with_repeated_elements() {
+        let items = vec![8, 2, 4, 6, 5, 7, 10, 8];
+        let result = quicksort(&items);
+
+        assert_eq!(result, vec![2, 4, 5, 6, 7, 8, 8, 10]);
     }
 }
